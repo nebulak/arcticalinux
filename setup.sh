bdae741f4c11714cf89d56fd9685ceb5657f5854
nebulak <nebulak@users.noreply.github.com>
2018-03-20 18:01:42 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Add new wallpaper
+
+# edit packages-desktop
+# /usr/share/manjaro-tools/iso-profiles/manjaro/xfce/Packages-Desktop
+tor
+tor-browser
+keepassxc
+# todo
+gajim
+aur/gajim-plugin-omemo-gtk3 
